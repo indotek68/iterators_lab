2124d6408901f08269ad440747ee7cca229e5756
indotek68 <angga.w.putra@gmail.com>
2014-07-16 00:24:45 -0700

    refactor

--- src/refactor.js
+++ src/refactor.js
@@ -11,3 +11,18 @@ var sqrRoot = function(num) {
 
 var sqrNumbers = map(myNumbers, square);
 var absNumbers = map(sqrNumbers, sqrRoot);
+
+//////////// refactor /////////////////////
+
+var myNumbers = [ -1, 2, -3, 4, -5, 6];
+
+var square = myNumbers.map(function(num) {
+    return num * num;
+});
+
+var sqrRoot = myNumbers.map(function(num) {
+    return Math.sqrt(num);
+});
+
+
+console.log("This is the square: " + square + " and this is the sqrt: " + sqrRoot);
